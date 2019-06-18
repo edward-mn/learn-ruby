86671c696467679a8bded63061be67fbfa69fb00
Edward Moreira <edward.moreira@yahoo.com>
2019-06-18 12:18:05 -0300

    Remove caracter especiais em variaveis

--- OneBit/hello_onebitcode.rb
+++ OneBit/hello_onebitcode.rb
@@ -5,14 +5,14 @@ print "Digite o segundo número: "
 num_dois = gets.chomp.to_i
 
 Soma = num_um + num_dois
-Subtração = num_um - num_dois 
-Multiplicação = num_um * num_dois
-Exponenciação = num_um ** num_dois
-Divisão = num_um / num_dois
+Subtracao = num_um - num_dois 
+Multiplicacao = num_um * num_dois
+Exponenciacao = num_um ** num_dois
+Divisao = num_um / num_dois
 Modulo = num_um % num_dois
  
-texto_completo = "Soma = #{Soma}, Subtração = #{Subtração}, Multiplicação = #{Multiplicação}," + 
-                    "Exponenciação = #{Exponenciação}, Divisão = #{Divisão}, Módulo = #{Modulo}"
+texto_completo = "Soma = #{Soma}, Subtração = #{Subtracao}, Multiplicação = #{Multiplicacao}," + 
+                    "Exponenciação = #{Exponenciacao}, Divisão = #{Divisao}, Módulo = #{Modulo}"
 puts 'O resultado da operação foi: ' << texto_completo
 
 
