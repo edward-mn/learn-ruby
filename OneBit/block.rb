a7fde367e0b3dabecdae795d80295a477870df44
Edward Moreira <edward.moreira@yahoo.com>
2019-06-21 16:46:41 -0300

    Primeiros contatos com blocos

---
+++ OneBit/block.rb
@@ -0,0 +1,46 @@
+5.times {puts 'Execução do bloco'}
+
+#Bloco com parametros
+soma = 0
+numbers = [5, 10, 15]
+numbers.each {|number| soma += number}
+puts soma
+
+#Blocos com múltiplas linhas
+hash_simples = { 1 => 2, 3 => 4}
+
+hash_simples.each do |key, value|
+  puts "Key = #{key}"
+  puts "value #{value}"
+  puts "key * value = #{key * value}"
+  puts '---'
+end
+
+#criando método que recebe um bloco como parametro
+def bloco_generico
+  #chama o bloco
+  yield
+  yield
+end
+
+bloco_generico {puts 'Meu bloco genérico'}
+
+#Passando método opcionalmente
+def bloco_generico_opcionalmente
+  if block_given?
+    #chama o bloco
+    yield
+  else
+    puts 'Sem parâmetro na chamada do método'
+  end
+end
+
+bloco_generico_opcionalmente
+bloco_generico_opcionalmente{ puts 'Com parâmetro na chamada do método'}
+
+def bloco_metodo(name, &block) #Só podemos passar um bloco por método
+  @name = name #Variáveis internas funcionam pois o bloco esta encapsulado dentro do método 
+  block.call #chama para executar
+end
+
+bloco_metodo('Edward') {puts "Olá #{@name}"}
