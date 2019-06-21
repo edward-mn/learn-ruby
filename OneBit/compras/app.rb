5a7f3665f5865ba029554a7e918dde6df3953472
Edward Moreira <edward.moreira@yahoo.com>
2019-06-21 15:54:02 -0300

    Finaliza mini projeto - Orientaçao a objetos simples ruby puro

---
+++ OneBit/compras/app.rb
@@ -0,0 +1,12 @@
+require_relative 'produto.rb'
+require_relative 'mercado.rb'
+
+puts '- Bem-vindo a vendinha do Edward -'
+print 'Digite o nome do produto que deseja comprar: '
+nome_produto = gets.chomp
+print 'Agora digite seu preço: '
+preco_produto = gets.chomp.to_f
+
+produto_novo = Produto.new(nome_produto, preco_produto)
+meu_mercado = Mercado.new(produto_novo)
+meu_mercado.comprar
---
+++ OneBit/compras/mercado.rb
@@ -0,0 +1,13 @@
+class Mercado < Produto
+  def initialize (produto)
+    @produto = produto
+  end
+
+  def comprar
+    unless @produto.nome == '' || @produto.preco == 0
+      puts "Você comprou o produto #{@produto.nome.capitalize} no valor de R$: #{@produto.preco}"
+    else
+      puts 'Desculpe não oferecemos produto de graça ou produtos sem nome!'
+    end
+  end
+end
