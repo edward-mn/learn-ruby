c906ec631259a96b572aa0fd2f9fde8a7ac04f1d
Edward Moreira <edward.moreira@yahoo.com>
2019-07-05 16:50:50 -0300

    Class number - 54

--- Udemy/RubyOnRails5-DoInicioAoFim/crypto_wallet/app/helpers/application_helper.rb
+++ Udemy/RubyOnRails5-DoInicioAoFim/crypto_wallet/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def br_date(us_date)
+    us_date.strftime('%d/%m/%Y')
+  end
+
+  def ambiente_rails
+    if Rails.env.development?
+      'Desenvolvimento'
+    elsif Rails.env.production?
+      'Produção'
+    else
+      'Teste'
+    end
+  end
 end
--- Udemy/RubyOnRails5-DoInicioAoFim/crypto_wallet/config/routes.rb
+++ Udemy/RubyOnRails5-DoInicioAoFim/crypto_wallet/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+
   resources :coins
+  get '/begin', to: 'coins#index'
+  #get '/coins', to: 'coins#idex' one by one
+
+  root to: 'welcome#index' #Default port
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
