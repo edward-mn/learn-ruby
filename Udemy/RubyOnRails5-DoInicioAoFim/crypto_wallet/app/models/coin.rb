525ecde064e270d5396b126872f72d794e51fb63
Edward Moreira <edward.moreira@yahoo.com>
2019-08-01 15:32:05 -0300

    Class number - 74 / module 3

--- Udemy/RubyOnRails5-DoInicioAoFim/crypto_wallet/app/models/coin.rb
+++ Udemy/RubyOnRails5-DoInicioAoFim/crypto_wallet/app/models/coin.rb
@@ -1,2 +1,4 @@
 class Coin < ApplicationRecord
+  belongs_to :mining_type
+  #Belongs_to fica do lado de onde tem a ForeignKey
 end
--- Udemy/RubyOnRails5-DoInicioAoFim/crypto_wallet/app/models/mining_type.rb
+++ Udemy/RubyOnRails5-DoInicioAoFim/crypto_wallet/app/models/mining_type.rb
@@ -1,2 +1,4 @@
 class MiningType < ApplicationRecord
+  has_many :coins
+  #Has_many fica do lado de onde não tem a ForeignKey mas que tem uma refência na outra
 end
