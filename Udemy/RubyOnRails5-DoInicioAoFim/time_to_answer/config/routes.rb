3e2ac5ff5aa7eb584e479ffebcabb5687b606f1c
Edward Moreira <edward.moreira@yahoo.com>
2019-09-16 12:39:24 -0300

    Class number - 150/module 5
    Fixing Edit route to Admin and Create Edit view - Done

--- Udemy/RubyOnRails5-DoInicioAoFim/time_to_answer/config/routes.rb
+++ Udemy/RubyOnRails5-DoInicioAoFim/time_to_answer/config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins, only: [:index] # Administradores
+    resources :admins, only: %i[index edit]
+    # resources :admins, only: [:index, :edit] # Administradores
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
