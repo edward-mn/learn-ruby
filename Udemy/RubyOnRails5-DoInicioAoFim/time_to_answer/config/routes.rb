c8e0611b01e1fe55eedfed5e38cfa499076d1529
Edward Moreira <edward.moreira@yahoo.com>
2019-09-16 12:18:35 -0300

    Fixing a problem in table admin view

--- Udemy/RubyOnRails5-DoInicioAoFim/time_to_answer/config/routes.rb
+++ Udemy/RubyOnRails5-DoInicioAoFim/time_to_answer/config/routes.rb
@@ -13,11 +13,6 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
     resources :admins, only: [:index] # Administradores
-=begin
-    get 'admins/index'
-    get 'admins/edit/:id', to: 'admins#edit'
-    Criando os resources a mão
-=end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
