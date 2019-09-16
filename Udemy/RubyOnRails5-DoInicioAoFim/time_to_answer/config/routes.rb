a874f531c8f5d1bc67e095f693321ca1dd1d364a
Edward Moreira <edward.moreira@yahoo.com>
2019-09-16 10:35:44 -0300

    Class number - 149/module 5
    Starts using resources in admins path instead of doing it manually - Done

--- Udemy/RubyOnRails5-DoInicioAoFim/time_to_answer/config/routes.rb
+++ Udemy/RubyOnRails5-DoInicioAoFim/time_to_answer/config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
   devise_for :users
+  devise_for :admins
+
   namespace :site do
     get 'welcome/index'
   end
+
   namespace :users_backoffice do
     get 'welcome/index'
   end
-  namespace :admins_backoffice do
-    get 'welcome/index'
-  end
 
-  devise_for :admins
+  namespace :admins_backoffice do
+    get 'welcome/index' # Dashboard
+    resources :admins, only: [:index] # Administradores
+=begin
+    get 'admins/index'
+    get 'admins/edit/:id', to: 'admins#edit'
+    Criando os resources a mão
+=end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'inicio', to: 'site/welcome#index'
