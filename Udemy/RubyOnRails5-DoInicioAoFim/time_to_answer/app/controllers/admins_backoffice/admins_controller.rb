6e197a2f55dd6caeced8486c82a94a8655903fd0
Edward Moreira <edward.moreira@yahoo.com>
2019-09-18 12:35:05 -0300

    Class number - 155/module 5
    Refatoring admin controller and Fixing path logout button - Done

--- Udemy/RubyOnRails5-DoInicioAoFim/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ Udemy/RubyOnRails5-DoInicioAoFim/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -1,24 +1,40 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verify_password, only: [:update]
+  before_action :set_admin, only: %i[edit update]
+
   def index
     @admins = Admin.all
   end
 
   def edit
-    @admin = Admin.find(params[:id])
+    #
   end
 
   def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-      params[:admin].extract!(:password, :password_confirmation)
-    end
-
-    @admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: 'Administrador Atualizado'
     else
       render :edit
+    end
+  end
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def verify_password
+    check_admins_params(:password) && check_admins_params(:password_confirmation)
+  end
+
+  def check_admins_params(param)
+    if params[:admin][param].blank?
+      params[:admin].extract!(param)
     end
   end
 end
